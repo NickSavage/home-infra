e28b65ee615f493af110924ca0cd7d47a1c28c46
Nick Savage <nick@nicksavage.ca>
2025-02-10 18:26:06 -0500

    adding two more nodes for garage

---
+++ hosts/thelio/thelio.tf
@@ -0,0 +1,67 @@
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "docker" {
+  alias = "thelio"
+  host = "ssh://nick@192.168.0.30:22"
+}
+
+
+variable "garage_rpc_secret" {
+  type        = string
+  description = "The secret for the garage RPC"
+}
+
+variable "garage_rpc_host" {
+  type = string
+  description = "The host's IP"
+  default = "192.168.0.30"
+}
+
+resource "docker_image" "garage" {
+  provider = docker.thelio
+  name = "dxflrs/garage:v1.0.0"
+  keep_locally = false
+}
+
+resource "docker_container" "garage" {
+  provider = docker.thelio
+  name = "garaged"
+  image = docker_image.garage.image_id
+  
+  upload {
+    content = templatefile("${path.module}/../../templates/garage/garage.toml.tpl", {
+      rpc_secret = var.garage_rpc_secret
+      rpc_host = var.garage_rpc_host
+    })
+    file = "/etc/garage.toml"
+  }
+
+  volumes {
+    host_path      = "/media/garaged/meta"
+    container_path = "/var/lib/garage/meta"
+  }
+  volumes {
+    host_path      = "/media/garaged/data"
+    container_path = "/var/lib/garage/data"
+  }
+  
+  ports {
+    internal = 3900
+    external = 3900
+  }
+  ports {
+    internal = 3901
+    external = 3901
+  }
+  ports {
+    internal = 3902
+    external = 3902
+  }
+}
--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ module "selena" {
   source = "./hosts/selena"
   garage_rpc_secret = var.garage_rpc_secret
 }
+
+module "harmonia" {
+  source = "./hosts/harmonia"
+  garage_rpc_secret = var.garage_rpc_secret
+}
+
+module "thelio" {
+  source = "./hosts/thelio"
+  garage_rpc_secret = var.garage_rpc_secret
+}
